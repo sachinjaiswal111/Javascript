1b8d2afe548a2606c810153453d772f25c936138
sachinjaiswal111 <155886953+sachinjaiswal111@users.noreply.github.com>
2025-02-25 13:27:06 +0000

    varible

---
+++ BasicsofJavascript/varible.js
@@ -0,0 +1,17 @@
+const accountId=144553;
+let accounEmail="sachin@krmu.in";
+var accountPassword="12345";
+accountcity="jaipur";
+// accountId=2;
+accountPassword="212212";
+accountEmail="sachin@gd.in";
+accountcity="delhi";
+
+let accountState;
+console.log([accountPassword,accountcity,accountEmail,accountState])
+
+console.log(accountId);
+
+/*
+prefer not to use var because of issue of 
+*/
