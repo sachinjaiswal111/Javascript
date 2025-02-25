6e1029e16c30746f2d3d52010b51c2db54cfa55f
sachinjaiswal111 <155886953+sachinjaiswal111@users.noreply.github.com>
2025-02-25 14:28:59 +0000

    conversion of dataytypes

---
+++ BasicsofJavascript/conversionAndOperation.js
@@ -0,0 +1,34 @@
+let score=null
+
+console.log(typeof(score))
+
+let valueInNumber=Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);// have nan when string is there or undefined// give 0 for null
+
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; flase=>0
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=>true; 0=>false
+
+//"" => false
+// "hitesh"=>true
+
+let someNumber=33;
+let stringNumber=String(33);
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
+
+
+
+
+
+
