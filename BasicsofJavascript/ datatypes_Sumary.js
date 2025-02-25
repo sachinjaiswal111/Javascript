4a4956ed79b0176f9b4b451de54d7d97697a594e
sachinjaiswal111 <155886953+sachinjaiswal111@users.noreply.github.com>
2025-02-25 19:03:14 +0000

    sumary

---
+++ BasicsofJavascript/ datatypes_Sumary.js
@@ -0,0 +1,34 @@
+// Datatypes:- 
+
+// Primitive:- 
+//: String,Number,Boolean,null,undefined,Symbol,BIG INT
+
+
+// Reference type(  Non Primitive)
+
+//Array,Objects,Functions
+// typeof operator give us object except function which give it function.which is also called object function.
+
+// dynamically typed: no need to specify data type
+
+const id=Symbol("123");
+const anotherid=Symbol("123");
+// both are not same symbool is used to assgin unique element.
+
+const binNumber=1234567893093n;
+
+// javascript stores in bigint
+
+const heros=["saktiman", "naagraj","doga"];
+
+let myobj={
+    name:"sachin",
+    age:22
+
+}
+
+const myfunc=function(){
+    console.log("hello world")
+}
+
+
