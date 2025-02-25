68cc0345134cb590dd42cda3576e826348a30908
sachinjaiswal111 <155886953+sachinjaiswal111@users.noreply.github.com>
2025-02-25 14:05:18 +0000

    datatypes

---
+++ BasicsofJavascript/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";// treat all js code as newer version
+
+// alert(3+3);  this alert is browser element.
+
+console.log(3+3)
+
+console.log("sachin");
+
+let name="sachin" //string
+let age=19 // number
+let isLoggedIn=false
+
+//number => 2 to power 53
+// bigint => 2 to power 53
+// string => ""
+// boolean => true/false
+//null => standalone value of empty
+//undefined => not defined yet after declaretion 
+//symbol => unique
+
+//  object
+
+console.log(typeof undefined)// undefined
+
+console.log(typeof null)// object
