a291d6c89f69f84590907c9d3b336bc0af7e9e10
sachinjaiswal111 <155886953+sachinjaiswal111@users.noreply.github.com>
2025-02-25 14:47:43 +0000

    tricky operation

--- BasicsofJavascript/conversionAndOperation.js
+++ BasicsofJavascript/conversionAndOperation.js
@@ -3,8 +3,8 @@ let score=null
 console.log(typeof(score))
 
 let valueInNumber=Number(score);
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);// have nan when string is there or undefined// give 0 for null
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);// have nan when string is there or undefined// give 0 for null
 
 
 //"33"=>33
@@ -14,7 +14,7 @@ console.log(valueInNumber);// have nan when string is there or undefined// give 
 let isLoggedIn=1
 
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1=>true; 0=>false
 
@@ -23,8 +23,48 @@ console.log(booleanIsLoggedIn);
 
 let someNumber=33;
 let stringNumber=String(33);
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ***********************operation***********
+
+let value=3
+let negValue=-value;
+console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello"
+let str2=" Sachih"
+
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");// boolean conversin come
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gamecounter=100
+gamecounter++
+console.log(gamecounter);
+
+
+
+
 
 
 
