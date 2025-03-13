846e609e71d5ad6da566ee8fde67e5b5624ab2bc
sachinjaiswal111 <155886953+sachinjaiswal111@users.noreply.github.com>
2025-03-13 05:12:11 +0000

    revise

--- BasicsofJavascript/ datatypes_Sumary.js
+++ BasicsofJavascript/ datatypes_Sumary.js
@@ -36,67 +36,69 @@
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
+// console.log(typeof(score));
+// console.log(typeof(scoreValue))
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-// console.log(id === anotherId);
+// // console.log(id === anotherId);
 
-const bigNumber = 3456543576654356754n
+// const bigNumber = 3456543576654356754n
 
 
 
-// Reference (Non primitive)
+// // Reference (Non primitive)
 
-// Array, Objects, Functions
+// // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
-    name: "hitesh",
-    age: 22,
-}
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }
 
-const myFunction = function(){
-    console.log("Hello world");
-}
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
 
-// console.log(typeof anotherId);
+// // console.log(typeof anotherId);
 
-console.log(typeof 100);
-console.log(typeof isLoggedIn);
-console.log(typeof null );
-console.log(typeof 100.3);
-console.log(typeof heros)
-console.log(typeof myObj)
-console.log(typeof myFunction);
+// console.log(typeof 100);
+// console.log(typeof isLoggedIn);
+// console.log(typeof null );
+// console.log(typeof 100.3);
+// console.log(typeof heros)
+// console.log(typeof myObj)
+// console.log(typeof myFunction);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
 
-// Stack(Primitive),Heap(Non-primitive)
+// // Stack(Primitive),Heap(Non-primitive)
 
 
-let gameName="Sachin";
-let anothername=gameName;
-anothername="sachin kumar" // gameName still not change
+// let gameName="Sachin";
+// let anothername=gameName;
+// anothername="sachin kumar" // gameName still not change
 
-let userOne={
-    email:"user@google.com",
-    upi:"user@ybl"
+// let userOne={
+//     email:"user@google.com",
+//     upi:"user@ybl"
 
-}
-let userTwo=userOne;
-// now if we change the user two name then user one is chagne.
-userTwo.email="userOne.email";
-console.log(userTwo.email);
-
-
+// }
+// let userTwo=userOne;
+// // now if we change the user two name then user one is chagne.
+// userTwo.email="userOne.email";
+// console.log(userTwo.email);
 
 
-console.log(anothername);
+
+
+// console.log(anothername);
--- BasicsofJavascript/conversionAndOperation.js
+++ BasicsofJavascript/conversionAndOperation.js
@@ -1,66 +1,66 @@
-let score=null
+// let score=null
 
-console.log(typeof(score))
+// console.log(typeof(score))
 
-let valueInNumber=Number(score);
-// console.log(typeof(valueInNumber));
-// console.log(valueInNumber);// have nan when string is there or undefined// give 0 for null
-
-
-//"33"=>33
-//"33abc"=>NaN
-//true=>1; flase=>0
-
-let isLoggedIn=1
-
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
-
-// 1=>true; 0=>false
-
-//"" => false
-// "hitesh"=>true
-
-let someNumber=33;
-let stringNumber=String(33);
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+// let valueInNumber=Number(score);
+// // console.log(typeof(valueInNumber));
+// // console.log(valueInNumber);// have nan when string is there or undefined// give 0 for null
 
 
-// ***********************operation***********
+// //"33"=>33
+// //"33abc"=>NaN
+// //true=>1; flase=>0
 
-let value=3
-let negValue=-value;
-console.log(negValue)
+// let isLoggedIn=1
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2/3);
-// console.log(2%3);
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn);
 
-let str1="hello"
-let str2=" Sachih"
+// // 1=>true; 0=>false
 
-let str3=str1+str2
-console.log(str3);
+// //"" => false
+// // "hitesh"=>true
 
-console.log("1"+2)
-console.log(1+"2")
-console.log("1"+2+2)
-console.log(1+2+"2");
+// let someNumber=33;
+// let stringNumber=String(33);
+// // console.log(stringNumber);
+// // console.log(typeof stringNumber);
 
-console.log(+true);
-console.log(+"");// boolean conversin come
 
-let num1,num2,num3
+// // ***********************operation***********
 
-num1=num2=num3=2+2
+// let value=3
+// let negValue=-value;
+// console.log(negValue)
 
-let gamecounter=100
-gamecounter++
-console.log(gamecounter);
+// // console.log(2+2);
+// // console.log(2-2);
+// // console.log(2*2);
+// // console.log(2**3);
+// // console.log(2/3);
+// // console.log(2%3);
+
+// let str1="hello"
+// let str2=" Sachih"
+
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");// boolean conversin come
+
+// let num1,num2,num3
+
+// num1=num2=num3=2+2
+
+// let gamecounter=100
+// gamecounter++
+// console.log(gamecounter);
 
 
 
