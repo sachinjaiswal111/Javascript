91019cf830f14511c71d67c2cfcb2c6a4b95f0c6
sachinjaiswal111 <155886953+sachinjaiswal111@users.noreply.github.com>
2025-03-13 08:35:46 +0000

    string

---
+++ BasicsofJavascript/05_string.js
@@ -0,0 +1,35 @@
+const name="sachin";
+const repoCount=50;
+//console.log(name+repoCount)
+// console.log(`Hello my name is ${name} and my repoCount ${repoCount}`);
+
+// another way define
+const gameName=new String('Sachin');
+// console.log(gameName[0]);
+// // console.log(gameName.length)
+
+// // console.log(gameName.toUpperCase());
+// // console.log(gameName.charAt(0));
+// // console.log(gameName.indexOf("S"));
+
+// console.log(gameName.charCodeAt(2));// return ascii value of charat 2;
+
+// console.log(gameName.concat(" and my repo count is  ", repoCount))
+
+const str1="is this a question";
+// console.log(str1.endsWith("n"));// gives true
+// console.log(str1.endsWith("tio"));// false;
+
+// console.log(str1.includes("this")); check for given word ins sentence
+// console.log(str1.includes("sachin"));
+// const surrogate1="\uD834";// this is half
+// let surro="\uD834\uDD1E";
+// console.log(surrogate1.isWellFormed());
+// console.log(surro.isWellFormed());
+// console.log(surro);
+// console.log(surrogate1);
+
+console.log(str1.lastIndexOf("this")); // return the index of first occruance this firstletter if presnet
+
+
+
